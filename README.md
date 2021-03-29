4d6be44f8796218ee2075fb708abda34b11bb06f
diveforme <diveforme@gmail.com>
2021-03-29 19:51:21 +0900

    1차 테트리스 레이아웃 완료

---
+++ README.md
@@ -0,0 +1,4 @@
+## HTML, Vanilla Javascript 테트리스 만들기
+
+보통 HTML의 Canvas를 사용해서 테트리스를 만든다. 그러나 이 프로젝트에서는 캔버스를 사용하지 않고, HTML의 ul과 li 를 사용하여 테트리스를 만든다.
+
---
+++ js/tetris.js
@@ -0,0 +1,17 @@
+// DOM
+const playground = document.querySelector(".playground > ul");
+console.log(playground);
+
+// ul 안에 li 집어넣기
+for(let i=0; i<20; i++){
+    const li = document.createElement("li");
+    const ul = document.createElement("ul");
+
+    for(let j=0; j<10; j++){
+        const matrix = document.createElement("li");
+        ul.prepend(matrix);
+    }
+
+    li.prepend(ul);
+    playground.prepend(li);
+}
